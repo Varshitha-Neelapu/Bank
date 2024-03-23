db0e7c8ca27e03bd4bb50f57a50b5030239719bc
Neelapu Varshitha <101424994+Varshitha-Neelapu@users.noreply.github.com>
2024-03-23 18:33:34 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -20,12 +20,6 @@ st.sidebar.write("""
 st.write("This app is based on 16 inputs that predict wheather a customer will deposit or not? Using this app, a bank can identify specific customer segments; that will make deposits.")
 st.write("Please use the following form to get started!")
 
-# selecting age
-st.subheader("Select Customer's Age")
-selected_age = st.slider("Select Customer's Age", min_value = 18, max_value = 95, 
-                         step = 1, value = 41)    # Slider does not tolerate dtype value mismatch df.age.max() was thus not used.
-st.write("Selected Age:", selected_age)
-
 
 # selecting job
 st.subheader("Select Customer's Job")
@@ -41,6 +35,12 @@ def encode_job(selected_item):
 
 ### Using function for encoding
 selected_job = encode_job(selected_job)  
+
+# selecting age
+st.subheader("Select Customer's Age")
+selected_age = st.slider("Select Customer's Age", min_value = 18, max_value = 95, 
+                         step = 1, value = 41)    # Slider does not tolerate dtype value mismatch df.age.max() was thus not used.
+st.write("Selected Age:", selected_age)
 
 
 # selecting marital status
