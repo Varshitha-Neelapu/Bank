78f9c2073c7f8c13f034fcf701fab74759a6ba09
Neelapu Varshitha <101424994+Varshitha-Neelapu@users.noreply.github.com>
2024-03-23 18:47:14 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -25,20 +25,17 @@ st.write("Please use the following form to get started!")
 st.subheader("Customer Job")
 selected_job = st.selectbox("", df['job'].unique(), index = 3)
 st.write("Selected Job:", selected_job)
-
-
 ## Encode the job entered by user
 ### Declaring function for encoding
 def encode_job(selected_item):
     dict_job = {'admin.':0, 'blue-collar':1, 'entrepreneur':2, 'housemaid':3, 'management':4, 'retired':5, 'self-employed':6, 'services':7, 'student':8, 'technician':9, 'unemployed':10, 'unknown':11}
     return dict_job.get(selected_item, 'No info available')
-
 ### Using function for encoding
 selected_job = encode_job(selected_job)  
 
 # selecting age
 st.subheader("Customer Age")
-selected_age = st.slider("Select Customer's Age", min_value = 18, max_value = 95, 
+selected_age = st.slider( min_value = 18, max_value = 95, 
                          step = 1, value = 41)    # Slider does not tolerate dtype value mismatch df.age.max() was thus not used.
 st.write("Selected Age:", selected_age)
 
@@ -47,13 +44,11 @@ st.write("Selected Age:", selected_age)
 st.subheader("Customer Marital")
 selected_marital = st.radio("", df['marital'].unique())
 st.write("Selected Marital:", selected_marital)
-
 ## Encode the marital entered by user
 ### Declaring function for encoding
 def encode_marital(selected_item):
     dict_marital = {'divorced':0, 'married':1, 'single':2}
     return dict_marital.get(selected_item, 'No info available')
-
 ### Using function for encoding
 selected_marital = encode_marital(selected_marital)  
 
@@ -62,23 +57,15 @@ selected_marital = encode_marital(selected_marital)
 st.subheader("Customer Education")
 selected_education = st.radio("", df['education'].unique())
 st.write("Selected Education:", selected_education)
-
 ## Encode the education entered by user
 ### Declaring function for encoding
 def encode_education(selected_item):
     dict_education = {'primary':0, 'secondary':1, 'tertiary':2, 'unknown':3}
     return dict_education.get(selected_item, 'No info available')
-
 ### Using function for encoding
 selected_education = encode_education(selected_education)  
 
-
-# selecting balance
-st.subheader("Customer Balance")
-selected_balance = st.slider('', min_value = -6847,
-          max_value = 81204, step = 1, value = int(df.balance.mean()))
-st.write("Selected Customer Balance", selected_balance)      
-
+  
 
 # selecting housing status
 st.subheader("Customer Housing Status")
@@ -86,31 +73,24 @@ st.subheader("Customer Housing Status")
 selected_housing = st.radio("", df['housing'].unique(), 
                             index = 1, key = "1")
 st.write("Housing Status", selected_housing)
-
-
 ## Encode the housing entered by user
 ### Declaring function for encoding
 def encode_housing(selected_item):
     dict_housing= {'no':0, 'yes':1}
     return dict_housing.get(selected_item, 'No info available')
-
 ### Using function for encoding
 selected_housing = encode_housing(selected_housing)  
 
 
-
 # selecting loan status
 st.subheader('Loan Status')
-
 selected_loan = st.radio("", df['loan'].unique()[::-1], index = 1, key = "2")
 st.write("Selected Loan Status", selected_loan)
-
 ## Encode the loan entered by user
 ### Declaring function for encoding
 def encode_loan(selected_item):
     dict_loan= {'no':0, 'yes':1}
     return dict_loan.get(selected_item, 'No info available')
-
 ### Using function for encoding
 selected_loan = encode_loan(selected_loan)  
 
@@ -119,9 +99,8 @@ selected_loan = encode_loan(selected_loan)
 # st.title("Customer Contact")
 st.subheader("Customer Contact")
 
-selected_contact = st.selectbox("Select Contact", df['contact'].unique(), 
-                            index = 1)
-st.write("Selected Contact Type", selected_contact)
+selected_contact = st.selectbox(df['contact'].unique(), index = 1)
+st.write("Selected Contact Type:", selected_contact)
 
 ## Encode the contact entered by user
 ### Declaring function for encoding
@@ -133,25 +112,16 @@ def encode_contact(selected_item):
 selected_contact = encode_contact(selected_contact)  
 
 
-# selecting day
-st.subheader('Day')
-day_range = list(range(df['day'].min(), df['age'].max()))
-selected_day = st.selectbox('Select Day:',(day_range), index = 19)
-st.write('You selected:', selected_day)
-
-
 # selecting month
 st.subheader('Last Contact Month of Customer')
 selected_month = st.selectbox("Select Month", df['month'].unique(), 
                             index = 1)
 st.write("Selected Month", selected_month)
-
 ## Encode the month entered by user
 ### Declaring function for encoding
 def encode_month(selected_item):
     dict_month = {'apr':0, 'aug':1, 'dec':2, 'feb':3, 'jan':4, 'jul':5, 'jun':6, 'mar':7, 'may':8, 'nov':9, 'oct':10, 'sep':11}
     return dict_month.get(selected_item, 'No info available')
-
 ### Using function for encoding
 selected_month = encode_month(selected_month)  
 
@@ -179,7 +149,6 @@ selected_pdays = st.selectbox('Select pdays:',(pdays_range), index = 0)
 st.write('You selected:', selected_pdays)
 
 
-
 # selecting previous
 st.subheader('Number of Contacts Performed Before this Campaign')
 previous_range = list(range(df['previous'].min(), df['previous'].max()))
@@ -187,18 +156,15 @@ selected_previous = st.selectbox('Select previous:',(previous_range), index = 0)
 st.write('You selected:', selected_previous)
 
 
-
 # selecting poutcome
 st.subheader('Outcome of Previous Campaign') 
 selected_poutcome = st.radio("Select poutcome", df['poutcome'].unique(), index = 0)
 st.write("Selected poutcome:", selected_poutcome)
-
 ## Encode the poutcome entered by user
 ### Declaring function for encoding
 def encode_pout(selected_item):
     dict_pout = {'failure':0, 'other':1, 'success':2, 'unknown':3}
     return dict_pout.get(selected_item, 'No info available')
-
 ### Using function for encoding
 selected_poutcome = encode_pout(selected_poutcome) 
 
@@ -216,9 +182,9 @@ classifier = pkl.load(pickle_in)
 
 
 prediction = classifier.predict([[selected_age, selected_marital, 
-                                  selected_education, selected_balance,  
+                                  selected_education,  
                                   selected_housing, selected_loan, 
-                                  selected_day, selected_month, selected_poutcome, 
+                                  selected_month, selected_poutcome, 
                                   selected_duration, 
                                   selected_campaign, selected_pdays, selected_contact, 
                                   dur_pdays, contact_housing, selected_job]])
