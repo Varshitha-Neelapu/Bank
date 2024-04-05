41eaafde650f375b8d520ede3e7dc5788faf7dc8
Neelapu Varshitha <101424994+Varshitha-Neelapu@users.noreply.github.com>
2024-04-05 17:01:08 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -3,6 +3,17 @@ import streamlit as st
 import pickle as pkl
 import os
 
+# Hide the Streamlit sidebar menu
+hide_st_style = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+header {visibility: hidden;}
+</style>
+"""
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
+
 
 df = pd.read_csv("data/bank.csv")
 
